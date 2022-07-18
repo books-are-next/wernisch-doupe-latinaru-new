2d58dedd006d1ad13f5a7b6b1fab503411ed81b9
gabGodiskova <gabgodiskova@gmail.com>
2022-07-18 08:51:48 +0000

    Deploying to docs from @ books-are-next/wernisch-doupe-latinaru-new@2617755b8fb76863653e45ccff3dd09a3ac45b3b 🚀

--- epub2nb-output/content/doupe_latinaru_006.md
+++ epub2nb-output/content/doupe_latinaru_006.md
@@ -5,7 +5,15 @@ contentType: prose
 
 <section>
 
+<div class="centered">
+
+<div class="verse">
+
 _Dobrý vkus nakonec vždycky zabrání tomu,  
 aby bylo řečeno podstatné._
+
+</div>
+
+</div>
 
 </section>
--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-be10ec9';
+const CACHE = 'cache-2617755';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
